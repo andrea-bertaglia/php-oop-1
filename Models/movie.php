cd018470d3209c350ed0f9ecd74b78d0c96c0878
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-31 17:13:00 +0200

    feat: draft definition setter/getter methods

--- Models/movie.php
+++ Models/movie.php
@@ -10,6 +10,76 @@ class Movie
     private int $year;
     private string $language;
     private string $img_path;
-    private int $vote_avg;
-    private Genre $genres;
+    private float $vote_avg;
+    // private Genre $genres;
+
+    // setter/getter di id
+    public function setId($id_number)
+    {
+        $this->id = $id_number;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // setter/getter di title
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    // setter/getter di overview
+    public function setOverview($overview)
+    {
+        $this->overview = $overview;
+    }
+    public function get()
+    {
+        return $this->overview;
+    }
+
+    // setter/getter di year
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    // setter/getter di language
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    // setter/getter di img_path
+    public function setImgPath($img_path)
+    {
+        $this->img_path = $img_path;
+    }
+    public function getImgPath()
+    {
+        return $this->img_path;
+    }
+
+    // setter/getter di vote_avg
+    public function setVoteAvg($vote_avg)
+    {
+        $this->vote_avg = $vote_avg;
+    }
+    public function getVoteAvg()
+    {
+        return $this->vote_avg;
+    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
 // importo le classi
-// require_once __DIR__ './Models/movie.php';
-// require_once __DIR__ './Models/genre.php';
+require_once 'Models/movie.php';
+require_once 'Models/genre.php';
+
+// test inserimento dati
+$movie1 = new Movie;
+$movie1->setId(1001);
+$movie1->setTitle('Il Signore degli Anelli');
+$movie1->setOverview("Il Signore degli Anelli narra della missione di nove personaggi che compongono la Compagnia dell'Anello, partiti per distruggere il più potente Anello del Potere, un'arma che potrebbe rendere invincibile il suo malvagio creatore Sauron se tornasse nelle sue mani, dandogli il potere di dominare tutta la Terra di Mezzo.");
+$movie1->setYear(2001);
+$movie1->setLanguage('it');
+$movie1->setImgPath('https://copertine.hoepli.it/hoepli/xxl/505/1891/5051891155718.jpg');
+$movie1->setVoteAvg(4.5);
+
+
+var_dump($movie1);
